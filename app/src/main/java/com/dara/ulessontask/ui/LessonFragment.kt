72ba72866edec22983a844bbc7f11985e489c54a
oluwadara,abijo@gmail.com <oluwadara.abijo@gmail.com>
2021-01-27 17:56:09 +0100

    feat: Create chapters screen

---
+++ app/src/main/java/com/dara/ulessontask/ui/LessonFragment.kt
@@ -0,0 +1,15 @@
+package com.dara.ulessontask.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dara.ulessontask.R
+
+class LessonFragment : Fragment(R.layout.fragment_lesson) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
